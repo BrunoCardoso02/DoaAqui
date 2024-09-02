12d7ba1abd2024a5c31b728cffc65ff0a914999d
BrunoCardoso02 <bruno.cardoso02@outlook.com>
2024-09-02 00:09:37 -0300

    Primeiro commit do projeto, ambiente criado e configurado

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import { configDotenv } from 'dotenv';
+configDotenv();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Olá Doa Aqui!')
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Project running on port ${port}`)
+})
